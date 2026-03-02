f54871fae2756a9fd8c25048322130186ad7dc5a
Danila Poyarkov <dev@dannote.net>
2026-03-02 17:34:53 +0300

    Compute layouts after clipboard paste

--- src/stores/editor.ts
+++ src/stores/editor.ts
@@ -1597,6 +1597,7 @@ export function createEditorStore() {
           figma.blobs
         )
         if (created.length > 0) {
+          computeAllLayouts(graph)
           state.selectedIds = new Set(created)
           requestRender()
         }
